4bea95d7cf0cfe8d27ee69c5fbdbdf13c0c18a22
gotojmp <gotojmp@gmail.com>
2019-04-01 10:45:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,52 @@
-const iploc = require('./index')(8000);
+//const iploc = require('./index')(8000);
+
+
+let ips = [];
+for (let i=0;i<1000000;++i) {
+    let ip = (1+parseInt(Math.random()*250)) + '.' + (1+parseInt(Math.random()*250)) + '.' + (1+parseInt(Math.random()*250)) + '.' + (1+parseInt(Math.random()*250));
+    ips.push(ip);
+}
+
+let a=Date.now();
+for (let ip of ips)
+(ip2int(ip));
+console.log(Date.now()-a);
+a=Date.now();
+for (let ip of ips)
+(ip2int2(ip));
+console.log(Date.now()-a);
+
+
+
+
+
+
+
+function ip2int (ip) {
+    let [a, b, c, d] = ip.split('.');
+    return Number(a) * 16777216 + Number(b) * 65536 + Number(c) * 256 + Number(d);
+}
+
+function ip2int2 (ip) {
+    let i = 0;
+    let l = ip.length;
+    let num = '';
+    let res = 0;
+    let step = 24;
+    let b;
+    for (; i < l; ++i) {
+        b = ip[i];
+        if (b == '.') {
+            res += Number(num) << step;
+            step -= 8;
+            num = '';
+        } else {
+            num += b;
+        }
+    }
+    res += Number(num);
+    return res;
+}
 
 // console.log(scan([1,3,4,5,6,26,37,48,59], 5, 0));
 // console.log(scan([1,3,4,5,6,26,37,48,59], 5, 1));
@@ -43,4 +91,4 @@ async function test () {
     }
 }
 
-setTimeout(test, 100);
+//setTimeout(test, 100);
